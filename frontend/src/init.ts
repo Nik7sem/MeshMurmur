34dc546be61ad6da7cf1ac31532b88e88341f75e
Matvei <matveiyan@yandex.ru>
2025-08-20 02:21:27 +0300

    Change version

--- frontend/src/init.ts
+++ frontend/src/init.ts
@@ -6,7 +6,7 @@ import {askNotificationPermission} from "@/utils/notifications.ts";
 import {SecureStorageManager} from "@/utils/p2p-library/secureStorageManager.ts";
 import {getDefaultUserData} from "@/defaultContext/getDefaultUserData.ts";
 // init constants
-export const AppVersion = 'Alpha v3.5'
+export const AppVersion = 'Alpha v3.99'
 askNotificationPermission()
 
 // const passphrase = prompt("Enter passphrase to encrypt your keys!");
